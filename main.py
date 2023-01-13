52490c97bef5769d6f6828dbd1a5bbb9f22751c2
jessi68 <lyn990312@gmail.com>
2023-01-13 22:03:21 +0900

    app'

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     # register modules/blueprints
     app.register_blueprint(chat_gpt_route, url_prefix=f'/{chat_gpt_route_path}')
     # start app
-    app.run(port=3000, debug=True)
+    app.run(debug=True)
 
