07e6f76a3bb3c7845d562539577bab569c8ae2e3
jessi68 <lyn990312@gmail.com>
2023-01-13 21:53:51 +0900

    fix: app

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+app = Flask(__name__)
 
 if __name__ == '__main__':
-    app = Flask(__name__)
+    
     # register modules/blueprints
     app.register_blueprint(chat_gpt_route, url_prefix=f'/{chat_gpt_route_path}')
     # start app
